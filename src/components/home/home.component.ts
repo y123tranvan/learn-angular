57791acbd0f1638a0f1fc46161983ac8dd5ca331
tranvany <rencooking09202@gmail.com>
2024-05-08 09:40:32 +0700

    lesson-2-add-component-in-layout

--- src/components/home/home.component.ts
+++ src/components/home/home.component.ts
@@ -1,15 +1,19 @@
 import { Component } from '@angular/core';
-
+import { HousingLocationComponent } from '../../app/housing-location/housing-location.component';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [CommonModule , HousingLocationComponent],
   template: `
     <section>
     <form>
       <input type="text" placeholder="Filter by city">
       <button class="primary" type="button">Search</button>
     </form>
+  </section>
+  <section class='result'>
+  <app-housing-location></app-housing-location>
   </section>
   `,
   styleUrl: './home.component.css'
